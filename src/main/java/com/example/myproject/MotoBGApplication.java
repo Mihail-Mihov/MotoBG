b38793ed86311af2af88107d4ce74a4588f92825
Mihail-Mihov <ebayshop@mail.bg>
2023-01-15 14:56:05 +0200

    Project Rebuild

---
+++ src/main/java/com/example/myproject/MotoBGApplication.java
@@ -0,0 +1,13 @@
+package com.example.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoBGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoBGApplication.class, args);
+    }
+
+}
